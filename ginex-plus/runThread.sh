19699af2d89013c0871e4ba96364b1bf8afc37f4
liuyiiib <653611718@qq.com>
2023-12-12 10:09:12 +0800

    remove log file

--- ginex-plus/runThread.sh
+++
@@ -1,5 +0,0 @@
-for i in 1024 512 256;
-do
-	echo $i threads;
-	python3 ogbn_products_sage_ginex.py --verbose --train-only --ginex-num-threads=$i
-done
--- ginex-plus/run_cache.sh
+++
@@ -1,8 +0,0 @@
-start_gpu=0
-for i in 0.005 0.02 0.04 0.06 0.08;
-do
-    log_file='log_'$i'.txt'
-	python3 test_sample.py --prop=$i --gpu=$start_gpu > $log_file&
-    echo "dispatch $i"
-    start_gpu+=1
-done
